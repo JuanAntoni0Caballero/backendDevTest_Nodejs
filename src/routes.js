9453102b6bcd3ea32d45acd8ce364df1ed60dd20
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2025-05-15 11:02:45 +0200

    Created product endpoint flow

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-
 const router = Router();
+import ProductsRoutes from './products/routes.js'
 
 router.get("/", (req, res) => {
   res.json("All good in here");
 });
+
+router.use('/products', ProductsRoutes)
 
 export default router;
