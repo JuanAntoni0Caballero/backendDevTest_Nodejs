7ab2cc7b1a0cc04a3159fdd0e0453485291d45a7
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2025-05-15 12:00:52 +0200

    Created route to service productid/similarids

--- src/error-handling/index.js
+++ src/error-handling/index.js
@@ -5,15 +5,15 @@ export default function errorHandling(app) {
     res.status(404).json({ message: "This route does not exist" });
   });
 
-  app.use((err, req, res, next) => {
-    logger.error("ERROR", req.method, req.path, err)
 
-    if (!res.headersSent) {
-      res.status(500).json({
-        message: "Internal server error. Check the server console",
-      });
-    } else {
-      next(err);
-    }
+  app.use((err, req, res, next) => {
+    logger.error(`${req.method} ${req.path} - ${err.message}`);
+
+    const status = err.status || 500;
+    res.status(status).json({
+      errorMessage: err.status === 500
+        ? "Error interno del servidor"
+        : err.message
+    });
   });
 }
